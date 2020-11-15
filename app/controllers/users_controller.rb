3c700f8ef46ccfb38ab70d732d3660c9b1d674c5
Conor <conorjm89@gmail.com>
2020-11-15 21:01:38 +0000

    Added show page so events can be displayed for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    # @current_user = current_user
   end
 
   def new
     @user = User.new
-    # @current_user = current_user
   end
 
   def create
@@ -21,26 +19,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    # May have to change this to: @user = User.find(user_params[:id])
     @user = User.find(params[:id])
-    # @current_user = current_user
   end
-
-  # def destroy
-  #   # Removes user from session, not actually destroying the user
-  #   # Not linked to yet in index
-  #   session.delete(:current_user_id)
-  #   @_current_user = nil
-  #   redirect_to root_path
-  # end
 
   private
 
   def user_params
     params.require(:user).permit(:username)
   end
-
-  # def current_user
-  #   @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
-  # end
 end
