7fea0f20e33ed1775fe730356d218e1430fb18b3
Conor <conorjm89@gmail.com>
2020-11-17 16:24:22 +0000

    Fixed minor errors in model associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ApplicationRecord
   validates :event_date, presence: true
 
   belongs_to :creator, class_name: 'User'
-  has_many :attendees, through: :invitations, source: :attendee
   has_many :invitations, foreign_key: :created_event_id
+  has_many :attendees, through: :invitations, source: :attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
-  has_many :attended_events, through: :invitations
   has_many :invitations, foreign_key: :attendee_id
+  has_many :attended_events, through: :invitations, source: :created_event
 end
