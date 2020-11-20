6238d6b6118e79401cf85c1fa0be57c17ba3cad6
Conor <conorjm89@gmail.com>
2020-11-20 23:40:57 +0000

    Updated invitations controller for better code

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,14 +4,18 @@ class InvitationsController < ApplicationController
   end
 
   def create
-    @invite = Invitation.new
-    @invite.attendee_id = params[:invitation][:user_id]
-    @invite.created_event_id = params[:invitation][:event_id]
+    @invite = Invitation.new(invite_params)
 
     if @invite.save
       redirect_to root_path, notice: 'Invite sent!'
     else
       render :new
     end
+  end
+
+  private
+
+  def invite_params
+    params.require(:invitation).permit(:attendee_id, :created_event_id)
   end
 end
